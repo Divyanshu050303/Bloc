7b06fbf3adf24cf284823adcfcb9f158ebb98df9
Divyanshu050303 <divyanshusingh7060@gmail.com>
2024-06-10 18:16:15 +0530

    remove the unwanted import

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 // import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
-import 'package:tap/animation/animation_class.dart';
+// import 'package:tap/animation/animation_class.dart';
 import 'package:tap/animation/bouncing_ball.dart';
-import 'package:tap/animation/rotate.dart';
+// import 'package:tap/animation/rotate.dart';
 
 // import 'package:tap/infinite_list_app/simple_bloc_observer.dart';
 
