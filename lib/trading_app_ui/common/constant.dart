a9a6f815d28506950cf24a997da5afb2f3b1c288
Divyanshu050303 <divyanshusingh7060@gmail.com>
2024-06-12 17:31:33 +0530

    change the color

--- lib/trading_app_ui/common/constant.dart
+++ lib/trading_app_ui/common/constant.dart
@@ -61,8 +61,8 @@ class Constant {
   static const secondaryColor = Color.fromRGBO(255, 153, 0, 1);
   static const backgroundColor = Colors.white;
   static const Color greyBackgroundCOlor = Color(0xffebecee);
-  static var selectedNavBarColor = Colors.cyan[800]!;
-  static const unselectedNavBarColor = Colors.black87;
+  static var selectedNavBarColor = Colors.black;
+  static const unselectedNavBarColor = Colors.black54;
   static List<Map<String, dynamic>> settingOption = [
     {"icon": Icons.person_outline, "title": "Profile", "onPressed": () {}},
     {
