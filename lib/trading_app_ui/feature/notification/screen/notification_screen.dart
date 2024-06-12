bd94ce6feff27902cfa698a20af36f0cb2ece26b
Divyanshu050303 <divyanshusingh7060@gmail.com>
2024-06-12 18:00:09 +0530

    add single child scroll view

--- lib/trading_app_ui/feature/notification/screen/notification_screen.dart
+++ lib/trading_app_ui/feature/notification/screen/notification_screen.dart
@@ -10,38 +10,40 @@ class NotificationScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     MediaQueryData mediaQueryData = MediaQuery.of(context);
     return Scaffold(
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 40,
-          ),
-          const Text(
-            "Notification",
-            style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
-          ),
-          const CustomSearchBar(
-            hintText: "Search Notification",
-          ),
-          SizedBox(
-            height: mediaQueryData.size.height * 0.73,
-            width: mediaQueryData.size.width,
-            child: ListView.builder(
-                itemCount: Constant.marketCompanyList.length,
-                itemBuilder: (context, index) {
-                  return NotificationCard(
-                    stockName: Constant.marketCompanyList[index]["stockName"],
-                    color: Constant.marketCompanyList[index]["color"],
-                    companyName: Constant.marketCompanyList[index]
-                        ["compnayName"],
-                    price: Constant.marketCompanyList[index]["price"],
-                    pricePercentage: Constant.marketCompanyList[index]
-                        ["pricePercentage"],
-                    icon: Constant.marketCompanyList[index]["icon"],
-                    isProfit: Constant.marketCompanyList[index]["isProfit"],
-                  );
-                }),
-          )
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 40,
+            ),
+            const Text(
+              "Notification",
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
+            ),
+            const CustomSearchBar(
+              hintText: "Search Notification",
+            ),
+            SizedBox(
+              height: mediaQueryData.size.height * 0.73,
+              width: mediaQueryData.size.width,
+              child: ListView.builder(
+                  itemCount: Constant.marketCompanyList.length,
+                  itemBuilder: (context, index) {
+                    return NotificationCard(
+                      stockName: Constant.marketCompanyList[index]["stockName"],
+                      color: Constant.marketCompanyList[index]["color"],
+                      companyName: Constant.marketCompanyList[index]
+                          ["compnayName"],
+                      price: Constant.marketCompanyList[index]["price"],
+                      pricePercentage: Constant.marketCompanyList[index]
+                          ["pricePercentage"],
+                      icon: Constant.marketCompanyList[index]["icon"],
+                      isProfit: Constant.marketCompanyList[index]["isProfit"],
+                    );
+                  }),
+            )
+          ],
+        ),
       ),
     );
   }
